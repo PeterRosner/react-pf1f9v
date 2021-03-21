3440175ae44d1ff74272562dd0360964652ed769
PeterRosner <prosner97@gmail.com>
2021-03-21 20:43:26 +0000

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
+import employees from"./employees.json"
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <p>employees</p>
     </div>
   );
 }
